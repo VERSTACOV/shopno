36b63b6d553a8d34a876079d628a1506546ea5e5
VERSTACOV <76948553+VERSTACOV@users.noreply.github.com>
2021-05-01 12:09:13 +0300

    reload

--- js/script.js
+++ js/script.js
@@ -20,29 +20,3 @@ if (iconMenu) {
 	});
 }
 
-const menuLinks = document.querySelectorAll('.header__link[data-goto]');
-if (menuLinks.length > 0) {
-	menuLinks.forEach(menuLink => {
-		menuLink.addEventListener('click', onMenuLinkClick);
-	});
-	function onMenuLinkClick(e) {
-		const menuLink = e.target;
-		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-			const gotoBlock = document.querySelector(menuLink.dataset.goto); //получаем в константу этот объект
-			//выщитать положение этого объекта с учетом высоты шапки
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight; //создаем константу gotoBlockValue и с помощью встроенной функции getBoundingClientRect() точка топ, потому что хочу получить растояние от верха этого объекта.Яполучаю его место положение  на странице в пикселях, так же я добавляю количество прокрученых пикселей для этого используем pageYOffset (y-вертикаль) и отнимаем высоту шапки.
-
-			if (iconMenu.classList.contains('active')) {
-				document.body.classList.remove('lock');
-				iconMenu.classList.remove('active');
-				menuBody.classList.remove('active');
-			}
-
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior: "smooth"
-			});
-			e.preventDefault();
-		}
-	}
-}
